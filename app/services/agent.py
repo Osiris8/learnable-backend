35414bcddb91be3e6e2e65c12553444c633a6d48
Osiris8 <miganosiris8@gmaaail.com>
2025-08-31 17:56:35 +0200

    update agent name

--- app/services/agent.py
+++ app/services/agent.py
@@ -1,5 +1,5 @@
 from app.services.ollama import ollama_service
-def tutor_agent(prompt: str, model: str) -> str:
+def edu_mentor_agent(prompt: str, model: str) -> str:
     return ollama_service(f"""
     You are a Tutor Agent.
     If the prompt includes (course, lesson, learning), create a structured course (intro, outline, explanations, exercises, quiz, final project).
@@ -9,7 +9,7 @@ def tutor_agent(prompt: str, model: str) -> str:
     """, model=model)
 
 
-def sante_agent(prompt: str, model: str) -> str:
+def medi_assist_agent(prompt: str, model: str) -> str:
     return ollama_service(f"""
     You are a Health Agent.
     Provide general wellness advice, explain concepts clearly, but DO NOT give medical diagnoses.
@@ -18,7 +18,7 @@ def sante_agent(prompt: str, model: str) -> str:
     """, model=model)
 
 
-def agriculture_agent(prompt: str, model: str) -> str:
+def green_helper_agent(prompt: str, model: str) -> str:
     return ollama_service(f"""
     You are an Agriculture Agent.
     Provide tips about crops, farming techniques, soil, irrigation, and sustainable agriculture.
@@ -27,9 +27,9 @@ def agriculture_agent(prompt: str, model: str) -> str:
     """, model=model)
 
 agents = {
-    "tutor": tutor_agent,
-    "sante": sante_agent,
-    "agriculture": agriculture_agent
+    "tutor": edu_mentor_agent,
+    "health": medi_assist_agent,
+    "agriculture": green_helper_agent
 }
 
 
